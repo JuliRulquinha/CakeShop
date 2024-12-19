09668ad47bd6beb0cd8b8c1625288fdeca949ff3
JuliRulquinha <juli-felix@outlook.com>
2024-12-19 16:29:16 -0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'groceries-app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
